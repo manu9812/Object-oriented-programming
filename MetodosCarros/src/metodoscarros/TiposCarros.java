2dc351ea6ff32e2f97c7042a92ebbfdc620419a0
manu9812 <32127007+manu9812@users.noreply.github.com>
2018-04-06 10:09:11 -0500

    Object-oriented programming
    about car methods

---
+++ MetodosCarros/src/metodoscarros/TiposCarros.java
@@ -0,0 +1,31 @@
+package metodoscarros;
+
+/*
+ *
+
+/**
+ *
+ * @author manuela garcia
+4  abril 2018 
+ */
+public class TiposCarros {
+    //  aqui creare los diferentes tipos de metodos que ejecutaran los objetos 
+    // de la clase metodos carros 
+    void arrancarCarro (){ // método para arrancar el carro
+        System.out.println(" el auto ha arrancado");
+    }
+     void frenarCarro (){ // método para fenar el carro
+        System.out.println(" el auto ha frenado ");
+    }
+      void retrocederCarro (){ // método para retroceder el carro
+        System.out.println(" el auto ha retrocedido");
+    }
+       void acelerarCarro (){ // método para acelerar el carro
+        System.out.println(" el auto ha acelerado");
+    }
+        void apagarCarro (){ // método para apagar el carro
+        System.out.println(" el auto ha apagado");
+    }
+    
+    
+}
