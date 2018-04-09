aac9f55cf3fc050369eff07fabf406fbb19ce1cc
manu9812 <32127007+manu9812@users.noreply.github.com>
2018-04-08 23:10:05 -0500

    cart methods in python

---
+++ MetodosCarro/MetodosCarros.py
@@ -0,0 +1,21 @@
+class carro:
+
+#se crea la clase carro
+
+    def __init__(self) -> object:  #aqui creare los diferentes tipos de metodos que ejecutaran los objetos
+    #de la clase metodos carros
+        print("\n") # salto de linea
+
+    def arrancar(self):  # se crea el metodo arrancara carro
+        print("el auto ha arrancado") # imprime mensaje por consola cuando este mettodo es invocado
+    def  frenar (self):  # se crea el metodo frenar carro
+        print("el auto ha frenado")  # imprime mensaje por consola cuando este mettodo es invocado
+    def  retroceder (self):  # se crea el metodo retroceder carro
+        print("rl auto ha retrocedido") # imprime mensaje por consola cuando este mettodo es invocado
+    def acelerar (self): # se crea el metodo acelerar carro
+        print("el auto ha acelerado") # imprime mensaje por consola cuando este mettodo es invocado
+    def apagar (self): # se crea el metodo apagar carro
+        print("el auto se ha apagado") # imprime mensaje por consola cuando este mettodo es invocado
+
+
+
---
+++ MetodosCarro/iii.py
@@ -0,0 +1,40 @@
+from MetodosCarros import carro  # se invocan los metodos de la clase metodos carro
+
+camion = carro()  # se crea un nuevo objeto llamado camion
+print("--------------------------CAMION---------------------------")  # impresion por consola
+print("el camion realizara las siguientes acciones")  # impresion por consola
+camion.arrancar()  # se invoca el metodo arrancar carro
+camion.acelerar()  # se invoca el metodo acelerar carro
+camion.frenar()  # se invoca el metodo frenar carro
+camion.retroceder()  # se invoca el metodo retroceder carro
+camion.apagar()  # se invoca el metodo apagar carro
+
+
+campero = carro()  # se crea un segundo objeto llamado campero
+print("--------------------------CAMPERO----------- ----------------")  # impresion por consola
+print("el campero realizara las siguientes acciones")  # impresion por consola
+campero.arrancar()  # se invoca el metodo arrancar carro para el segundo objeto
+campero.acelerar()  # se invoca el metodo acelerar carro para el segundo objeto
+campero.frenar()  # se invoca el metodo frenar carro para el segundo objeto
+campero.retroceder()  # se invoca el metodo retroceder carro para el segundo objeto
+campero.apagar()  # se invoca el metodo apagar carro para el segundo objeto
+
+
+automovil = carro()  # se crea un tercer obejto llamado automovil
+print("--------------------------AUTOMOVIL---------------------------")  # impresion por consola
+print("el automovil realizara las siguientes acciones")  # impresion por consola
+automovil.arrancar()  # se invoca el metodo arrancar carro para el tercer objeto
+automovil.acelerar()  # se invoca el metodo acelerar carro para el tercer objeto
+automovil.frenar()  # se invoca el metodo frenar carro para el tercer objeto
+automovil.retroceder()  # se invoca el metodo retroceder carro para el tercer objeto
+automovil.apagar()  # se invoca el metodo apagar carro para el tercer objeto
+
+
+clasico = carro()
+print("--------------------------AUTO CLASICO---------------------------")
+print("el auto clasico realizara las siguientes acciones")
+clasico.arrancar()  # se invoca el metodo arrancar carro para el cuarto objeto
+clasico.acelerar()  # se invoca el metodo acelerar carro para el cuarto objeto
+clasico.frenar()  # se invoca el metodo frenar carro para el cuarto objeto
+clasico.retroceder()  # se invoca el metodo retroceder carro para el cuarto objeto
+clasico.apagar()  # se invoca el metodo apagar carro para el cuarto objeto
